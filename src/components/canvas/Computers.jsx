f85b71e868874b64f60d1dc96d47d4ea314dce7e
majdyaq <majd@feelit.tech>
2025-01-12 11:04:42 +0200

    3d section complete

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 import { Suspense, useEffect, useState } from "react";
 
@@ -5,7 +6,11 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
-const Computers = () => {
+
+const Computers = ({ isMobile }) => {
+  const pcScale = isMobile ? 0.5 : 0.6;
+  const pcPosition = isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5];
+
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -22,8 +27,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={pcScale}
+        position={pcPosition}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -31,6 +36,23 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, [isMobile]);
+
   return (
     <Canvas
       frameloop="demand"
@@ -44,7 +66,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={1.5}
           minPolarAngle={1.5}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>

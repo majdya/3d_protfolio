de9f9e8c2a3082b98fdd5edbe24de531c0b463b7
majdyaq <majd@feelit.tech>
2025-01-23 12:26:48 +0200

    works list section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import ProjectCard from "./ProjectCard";
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secoundry text-[17px] max-w-3xl leading-[30px]"
+        >
+          A detailed list of projects I have developed will be added shortly.
+        </motion.p>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Works
+const WCS = SectionWrapper(Works, "");
+export default WCS;
